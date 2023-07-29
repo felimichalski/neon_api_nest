ce599481810f3747ae81823fd505e308b619e256
Felipe Michalski <felimichalski@icloud.com>
2023-07-29 10:56:49 -0300

    product price by size

--- src/price/entities/price.entity.ts
+++ src/price/entities/price.entity.ts
@@ -2,29 +2,20 @@ import { Product } from 'src/products/entities/product.entity';
 import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Size {
+export class Price {
   @PrimaryGeneratedColumn()
   id?: number;
 
   @Column()
-  small_width: number;
+  small: number;
 
   @Column()
-  small_height: number;
+  medium: number;
 
   @Column()
-  medium_width: number;
+  large: number;
 
-  @Column()
-  medium_height: number;
-
-  @Column()
-  large_width: number;
-
-  @Column()
-  large_height: number;
-
-  @OneToOne(() => Product, (product) => product.size, {
+  @OneToOne(() => Product, (product) => product.unit_price, {
     onDelete: 'CASCADE',
   })
   product?: Product;
